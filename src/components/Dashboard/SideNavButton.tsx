0c9404fc4e02b62d3ffb7f1c4539198e93c9d749
jordanjunior22 <jordan.kheambo@final.edu.tr>
2025-04-02 13:37:53 +0100

    update

--- src/components/Dashboard/SideNavButton.tsx
+++ src/components/Dashboard/SideNavButton.tsx
@@ -17,8 +17,8 @@ const SidebarButton = ({ icon: Icon, name, href }: SidebarButtonProps) => {
     <Link
       href={href}
       className={`text-sm flex items-center w-full px-4 py-2 rounded-lg transition-all 
-                 text-foreground hover:text-background hover:bg-foreground hover:shadow-lg
-                 ${isActive ? "bg-foreground text-background shadow-lg" : ""}`}
+                 hover:text-background hover:bg-foreground hover:shadow-lg
+                 ${isActive ? "bg-foreground text-background shadow-lg" : "text-foreground"}`}
     >
       <Icon className="w-5 h-5 mr-2" />
       <span>{name}</span>
