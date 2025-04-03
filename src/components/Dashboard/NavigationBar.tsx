f1b5fc759a6eccccdb1b9a3a059e9c66bb938bac
Franck Elysee <franckelysee671@gmail.com>
2025-04-03 16:11:46 +0100

    wip

--- src/components/Dashboard/NavigationBar.tsx
+++ src/components/Dashboard/NavigationBar.tsx
@@ -76,7 +76,7 @@ export default function NavigationBar({ icon: Icon, baseHref, title, toggleSideb
         </button> */}
         <UserMenuModal
           avatarUrl="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"
-          userName="Franck Elysee"
+          userName="John Doe"
           onSignOut={handleSignOut}
           onToggleTheme={toggleTheme}
           isDarkMode={isDarkMode}
